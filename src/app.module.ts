4af7bd9e51c5784d6e28604665c8d1d1054d1f3e
den.lysenko <den.l@april33.com>
2018-05-22 18:31:37 +0300

    put db creds into .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,25 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+dotenv.config({
+  path: path.resolve(
+    process.cwd(),
+    `./src/config/${process.env.NODE_ENV}/.env`,
+  ),
+});
 
 @Module({
   imports: [
     GraphQLModule,
-    MongooseModule.forRoot('mongodb://mongodb/bookapp-dev'),
+    MongooseModule.forRoot(`mongodb://mongodb`, {
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASSWORD,
+      dbName: process.env.DB_NAME,
+    }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {

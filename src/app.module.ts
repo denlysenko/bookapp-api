e1e146282feb3e30ce4fb3c55120f9340efcbe74
den.lysenko <den.l@april33.com>
2018-05-22 14:31:29 +0300

    GraphQL setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule],
   controllers: [AppController],
-  providers: [ AppService ]
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(graphiqlExpress({ endpointURL: '/graphql' }))
+      .forRoutes('/graphiql')
+      .apply(graphqlExpress(req => ({ schema: {}, rootValue: req })))
+      .forRoutes('/graphql');
+  }
+}

56295af2b9e602435410873f5a4c5b748f61f235
Denis Lysenko <den.lysenko.mail@gmail.com>
2018-05-23 22:14:50 +0300

    comment graphql schema

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'users/interfaces/user.interface';
 
 import { Comment } from './interfaces/comment.interface';
 
@@ -9,18 +8,10 @@ import { Comment } from './interfaces/comment.interface';
 export class CommentsService {
   constructor(
     @InjectModel('Comment') private readonly commentModel: Model<Comment>,
-    @InjectModel('User') private readonly userModel: Model<User>,
   ) {}
 
   async getAllForBook(bookId: string) {
-    return await this.commentModel
-      .find({ bookId })
-      .populate({
-        path: 'messages.author',
-        select: 'displayName',
-        model: this.userModel,
-      })
-      .exec();
+    return await this.commentModel.find({ bookId }).exec();
   }
 
   async saveForBook(bookId: string, authorId: string, text: string) {

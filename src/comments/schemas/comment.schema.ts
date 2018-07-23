ed7e3f0981c9b314830d15e236d81ea4178f7a54
den.lysenko <den.l@april33.com>
2018-07-23 10:12:31 +0300

    createdAt for comments

--- src/comments/schemas/comment.schema.ts
+++ src/comments/schemas/comment.schema.ts
@@ -4,4 +4,8 @@ export const CommentSchema = new Schema({
   bookId: Schema.Types.ObjectId,
   authorId: Schema.Types.ObjectId,
   text: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
